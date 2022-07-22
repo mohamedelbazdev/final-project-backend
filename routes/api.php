104f2af49888d823f5c0732ae1b626c3194ab056
esraanabeh <esraanabeh92@gmail.com>
2022-07-22 18:54:04 +0200

    add category view
    create category view

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,7 @@ use App\Http\Controllers\Api\Admin\CategroyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Admin\OrderController;
-use App\Http\Controllers\Api\Admin\AuthController;
-use App\Http\Controllers\Api\Admin\CategroyController;
+
 use App\Http\Controllers\Api\Admin\ChatController;
 use App\Http\Controllers\Api\Admin\FeedbackController;
 use App\Http\Controllers\Api\Admin\PaymentController;
@@ -32,16 +31,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
+
 // login admin
 Route::post('login', [AuthController::class, 'login']);
 
 Route::apiResource('categories', CategroyController::class);
-=======
-Route::group(['namespace' => 'Api'], function () {
 
 
 
-
-});
->>>>>>> f991c6a7c5b7e9b4192cd9f2d8296b6ede4aa035
