162cb074fe12782ebae723db9fc218c681c49d72
esraanabeh <esraanabeh92@gmail.com>
2022-07-22 17:41:42 +0200

    api routes
    api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Admin\OrderController;
+use App\Http\Controllers\Api\Admin\AuthController;
+use App\Http\Controllers\Api\Admin\CategroyController;
+use App\Http\Controllers\Api\Admin\ChatController;
+use App\Http\Controllers\Api\Admin\FeedbackController;
+use App\Http\Controllers\Api\Admin\PaymentController;
+use App\Http\Controllers\Api\Admin\RateController;
+use App\Http\Controllers\Api\Admin\RateProviderController;
+use App\Http\Controllers\Api\Admin\RateUserController;
+use App\Http\Controllers\Api\Admin\UserController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +28,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'Api'], function () {
+
+
+
+
 });
