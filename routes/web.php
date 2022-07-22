4aaf8d04a2c1aaf04154d25aa216261dee095cd7
dev belal_Naeim <belal.naeem@yahoo.com>
2022-07-22 14:13:48 +0200

    new dashboard
    new dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/dashboard', function () {
-    return view('admin.index');
+    return view('table-basic');
 })->name('dashboard');
