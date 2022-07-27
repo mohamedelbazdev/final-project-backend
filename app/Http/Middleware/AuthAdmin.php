de116c6dd0a3929f0e621b56fc32c4a9fd863099
esraanabeh <esraanabeh92@gmail.com>
2022-07-27 22:26:38 +0200

    ..

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -16,7 +16,7 @@ class AuthAdmin {
     */
 
     public function handle( Request $request, Closure $next ) {
-        if ( Auth::check() ) {
+        if ( Auth::check() && Auth::user()->role == 1||2||3) {
             return $next( $request );
         }
         return route( 'admin.login' );
