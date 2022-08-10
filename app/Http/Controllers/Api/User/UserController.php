6c9420fa2207cf6cdc85ad20ba1288a1d91d7ae5
ahmednour2030 <a_mohamednour@yahoo.com>
2022-08-10 22:09:51 +0200

    update

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -63,7 +63,14 @@ class UserController extends Controller {
         // ->withCount('rateprovider')
         // ->whereId($request->post('user_id'))
         // ->first();
-        $provider=Provider::with('users')->whereUserId($request->post('user_id'))->with('categories')->withCount('rateprovider') ->first();
+        $provider=Provider::with(['users' => function($query){
+
+            $query->withCount('favorite');
+
+        }])->whereUserId($request->post('user_id'))
+            ->with('categories')
+            ->withCount('rateprovider')
+            ->first();
 
 
         return $this->apiResponse( 'successfully', $provider);
